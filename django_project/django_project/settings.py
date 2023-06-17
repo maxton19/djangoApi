5ac3794bc1bf7332c11060755444255c0b4d1780
rian19 <intendcourse@gmail.com>
2023-06-18 03:31:35 +0700

    add permissions

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = { # new
     "DEFAULT_PERMISSION_CLASSES": [
-           "rest_framework.permissions.AllowAny",
+           "rest_framework.permissions.IsAuthenticated",
 ],
 }
 
